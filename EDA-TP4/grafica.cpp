3e35817beb7139fa5d158d7cd871a81699f7a215
Francisco Basili <57078209+FranBasili@users.noreply.github.com>
2021-03-27 14:48:36 -0300

    Primeros pasos del front

--- EDA-TP4/grafica.cpp
+++ EDA-TP4/grafica.cpp
@@ -4,9 +4,20 @@
 #include <allegro5/allegro_image.h>
 #include <iostream>
 
+#define DISPLAYHEIGHT   470
+#define DISPLAYWIDTH    900
+#define FPS_ALLEGRO		50
+#define CANT_WALK_IMG	15
+
+#define ERROR   -1
+#define OK      0
+
 using namespace std;
 
 static ALLEGRO_DISPLAY* display;
+static ALLEGRO_EVENT_QUEUE* allegroQueue;
+static ALLEGRO_TIMER* timer;
+static ALLEGRO_BITMAP* bitmapBackground; //todo: Revisar. 
 
 // Retorna 1 si hay error, 0 si no lo hay
 bool initAllegro(void)
@@ -14,36 +25,68 @@ bool initAllegro(void)
     if (!al_init())
     {
         cout << "failed to initialize allegro!\n" << endl;
-        return 1;
+        return ERROR;
     }
 
     if (!al_init_image_addon())
     {
         cout << "failed to initialize images addon!\n" << endl;
-        return 1;
+        return ERROR;
     }
 
     if (!al_install_keyboard())
     {
         cout << "failed to initialize keyboard!\n" << endl;
-        return 1;
+        return ERROR;
     }
 
     display = al_create_display(DISPLAYWIDTH, DISPLAYHEIGHT);
+    
+    timer = al_create_timer(1.0/FPS_ALLEGRO);
 
-    return 0;
+    allegroQueue = al_create_event_queue();
+    al_register_event_source(allegroQueue, al_get_keyboard_event_source());
+    al_register_event_source(allegroQueue, al_get_display_event_source(display));
+    al_register_event_source(allegroQueue, al_get_timer_event_source(timer));
+
+    return OK;
 }
 
 void shutdownAllegro(void) {
 
     al_destroy_display(display);
-    al_destroy_event_queue(queue);
+    al_destroy_event_queue(allegroQueue);
     al_destroy_timer(timer);
     al_destroy_bitmap(bitmapBackground);
 
     al_shutdown_image_addon();
     al_uninstall_keyboard();
     al_uninstall_mouse();
+}
+
+bool loadWalkImages() 
+{
+    static ALLEGRO_BITMAP* walkingImg[CANT_WALK_IMG];
+
+    char ruta[30];
+
+    for (int i = 0; i < CANT_WALK_IMG; i++)
+    {
+        sprintf_s(ruta, "/wwalking/wwalk-F%d", (i+1));
+        walkingImg[i] = al_load_bitmap(ruta);
+
+        if (walkingImg[i] == NULL)
+        {
+            // Si la carga falla, cargamos 1=TRUE en error.
+            cout << "couldn't load" << ruta << endl;
+            return ERROR;
+        }
+    }
+    setWalkImages(walkingImg);
+}
+
+bool loadJumpImages()
+{
 }
 
 void redraw()
--- EDA-TP4/grafica.h
+++ EDA-TP4/grafica.h
@@ -1,9 +1,6 @@
 #ifndef GRAFICA
 #define GRAFICA
 
-#define DISPLAYHEIGHT   470
-#define DISPLAYWIDTH    900
-
 void shutdownAllegro(void);
 bool initAllegro(void);
 
